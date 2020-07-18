ac8e20bf19b0dd0cbfd6c48052efb171168b9be5
Tom Grant <thomas.grant.duck321@gmail.com>
2020-07-18 17:03:57 -0500

    chore: revert hacky tts fix

--- packages/ulangi-server/src/adapters/GoogleTextToSpeechAdapter.ts
+++ packages/ulangi-server/src/adapters/GoogleTextToSpeechAdapter.ts
@@ -1,23 +1,17 @@
-/*
- * Copyright (c) Minh Loi.
- *
- * This file is part of Ulangi which is released under GPL v3.0.
- * See LICENSE or go to https://www.gnu.org/licenses/gpl-3.0.txt
- */
-
-import { TextToSpeechClient } from '@google-cloud/text-to-speech';
-import { google } from '@google-cloud/text-to-speech/build/protos/protos';
+import TextToSpeech, {
+  SynthesizeSpeechResponse,
+} from '@google-cloud/text-to-speech';
 
 export class GoogleTextToSpeechAdapter {
   private textToSpeechClient: InstanceType<
-    typeof TextToSpeechClient
+    typeof TextToSpeech.TextToSpeechClient
   >;
 
   public constructor(
     googleCloudProjectId: string,
     googleCloudServiceAccount: string
   ) {
-    this.textToSpeechClient = new TextToSpeechClient({
+    this.textToSpeechClient = new TextToSpeech.TextToSpeechClient({
       projectId: googleCloudProjectId,
       keyFilename: googleCloudServiceAccount,
     });
@@ -40,7 +34,7 @@ export class GoogleTextToSpeechAdapter {
     text: string,
     languageCode: string,
     voiceName?: string
-  ): Promise<[google.cloud.texttospeech.v1.ISynthesizeSpeechResponse, google.cloud.texttospeech.v1.ISynthesizeSpeechRequest | undefined, {} | undefined]> {
+  ): Promise<[SynthesizeSpeechResponse]> {
     return this.textToSpeechClient.synthesizeSpeech({
       input: {
         text,
